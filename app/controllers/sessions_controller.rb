d698895113edc95df6ea4a51d47cfb3b21dbe720
Wangari-Sharon <wangarik35@gmail.com>
2022-10-07 07:55:37 +0300

    Controllers complete

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+          session[:user_id] = user.id
+          render json: user
+        else
+          # render json: {errors: user.errors}, status: :unauthorized
+          render json: {errors: ["Username or Password Incorrect"]}, status: :unauthorized
+        end
+      end
+    
+      def destroy
+        if session[:user_id] 
+          session.destroy
+          head :no_content
+        else
+          render json: {errors: ["No user logged in"]}, status: :unauthorized
+        end
+      end
+end
